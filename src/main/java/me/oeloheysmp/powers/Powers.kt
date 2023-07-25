1a164d81f29f30fcf0b2475cf418dbc3ec7aa102
Zings <hotzings@gmail.com>
2023-07-25 16:14:15 -0400

    converted to kotlin

--- src/main/java/me/oeloheysmp/powers/Powers.kt
+++ src/main/java/me/oeloheysmp/powers/Powers.kt
@@ -1,15 +1,19 @@
 package me.oeloheysmp.powers
 
 import me.oeloheysmp.powers.powers.goldenPaw.goldenPaw
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class Powers : JavaPlugin() {
 
     override fun onEnable() {
         instance = this
+        print("Powers plugin booting up")
 
         // Schedule a repeating task to run every 20 ticks (1 second)
-        server.scheduler.runTaskTimer(this, ::goldenPaw, 0, 20)
+        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, ::goldenPaw, 0, 20)
+
+        print("powers plugin loaded")
     }
 
     override fun onDisable() {
--- src/main/java/me/oeloheysmp/powers/utils/checkPlayerItems.kt
+++ src/main/java/me/oeloheysmp/powers/utils/checkPlayerItems.kt
@@ -1,30 +1,26 @@
-package me.oeloheysmp.powers.utils;
+package me.oeloheysmp.powers.utils
 
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.entity.Player
+import org.bukkit.inventory.Inventory
 
-public class checkPlayerItems {
-    public static boolean hasCustomItem(Player player, String Id) { // Checks if the player has the item in there inventory
-        Inventory inv = player.getInventory(); // get the players inventory
-
-        for (ItemStack item : inv.getContents()) { // loop through the content
-            if (item!=null){
-                String data = item.getItemMeta().getAsString(); // get nbt tags i think
-
-                if(data.contains("itemsadder:{id:\"" + Id + "\"")){ // check if it contains the items adder id for the item
-                    return true;
+object checkPlayerItems {
+    @JvmStatic
+    fun hasCustomItem(player: Player, Id: String): Boolean { // Checks if the player has the item in there inventory
+        val inv: Inventory = player.inventory // get the players inventory
+        for (item in inv.contents) { // loop through the content
+            if (item != null) {
+                val data = item.itemMeta!!.asString // get nbt tags i think
+                if (data.contains("itemsadder:{id:\"$Id\"")) { // check if it contains the items adder id for the item
+                    return true
                 }
             }
         }
-        return false; // return false if it failed the model check
+        return false // return false if it failed the model check
     }
 
-    public static boolean hasCustomItemInMainHand(Player player, String ID) { // checks if the player is holding the item
-        ItemStack item = player.getInventory().getItemInMainHand(); // get the players inventory
-
-        String data = item.getItemMeta().getAsString();
-
-        return data.contains("itemsadder:{id:\"" + ID + "\"");// return false if it failed the model check
+    fun hasCustomItemInMainHand(player: Player, ID: String): Boolean { // checks if the player is holding the item
+        val item = player.inventory.itemInMainHand // get the players inventory
+        val data = item.itemMeta!!.asString
+        return data.contains("itemsadder:{id:\"$ID\"") // return false if it failed the model check
     }
 }
